fdb48275aac6d68269e12bee2a0c2dfb28f18e8e
mkamei <mkamei@student.42tokyo.jp>
2022-05-02 18:36:53 +0900

    Edit isprime

--- go02/ex06/vendor/piscine/isprime.go
+++ go02/ex06/vendor/piscine/isprime.go
@@ -3,8 +3,12 @@ package piscine
 func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
+	} else if nb == 2 {
+		return true
+	} else if nb%2 == 0 {
+		return false
 	}
-	for x := 2; x*x/x == x && x*x <= nb; x++ {
+	for x := 3; x*x/x == x && x*x <= nb; x += 2 {
 		if nb%x == 0 {
 			return false
 		}
