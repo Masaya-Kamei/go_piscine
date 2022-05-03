be1fa5e9f6f1e784a1041e9f0ffa15b763e0cba6
Masaya Kamei <km9smc6@gmail.com>
2022-05-03 11:33:05 +0900

    Add ex07

---
+++ go03/ex07/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Capitalize("Hello! How are you? How+are+things+4you?"))
+
+	// fmt.Println(piscine.Capitalize("hELLO! How are you? How+are+things+4YOU?"))
+}
---
+++ go03/ex07/vendor/piscine/capitalize.go
@@ -0,0 +1,31 @@
+package piscine
+
+func isAlNum(r rune) bool {
+	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9')
+}
+
+func toUpperRune(r rune) rune {
+	if r >= 'a' && r <= 'z' {
+		return r - 32
+	}
+	return r
+}
+
+func toLowerRune(r rune) rune {
+	if r >= 'A' && r <= 'Z' {
+		return r + 32
+	}
+	return r
+}
+
+func Capitalize(s string) string {
+	rs := []rune(s)
+	for i := range s {
+		if i == 0 || !isAlNum(rs[i-1]) {
+			rs[i] = toUpperRune(rs[i])
+		} else {
+			rs[i] = toLowerRune(rs[i])
+		}
+	}
+	return string(rs)
+}
