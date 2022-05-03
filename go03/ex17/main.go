7118890bd73e219887410fcae04711e410766e25
Masaya Kamei <km9smc6@gmail.com>
2022-05-03 16:04:27 +0900

    Add ex17

---
+++ go03/ex17/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.AtoiBase("125", "0123456789"))
+	fmt.Println(piscine.AtoiBase("1111101", "01"))
+	fmt.Println(piscine.AtoiBase("7D", "0123456789ABCDEF"))
+	fmt.Println(piscine.AtoiBase("uoi", "choumi"))
+	fmt.Println(piscine.AtoiBase("bbbbbab", "-ab"))
+
+	// fmt.Println(piscine.AtoiBase("a", "ab"))
+	// fmt.Println(piscine.AtoiBase("", "ab"))
+	// fmt.Println(piscine.AtoiBase("125", "01234567890"))
+	// fmt.Println(piscine.AtoiBase("000", "0"))
+	// fmt.Println(piscine.AtoiBase("125", "+0123456789"))
+	// fmt.Println(piscine.AtoiBase("125a", "0123456789"))
+}
