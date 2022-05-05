d9bd41a321e677e9de8a8b4d1d90827929480cec
Masaya Kamei <km9smc6@gmail.com>
2022-05-05 14:06:56 +0900

    Add ex06

---
+++ go05/ex06/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	s := "HelloHAhowHAareHAyou?"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+
+	s = "HAHelloHAhowHAareHAyou?HA"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	s = "HelloHAHAHAHAHAhowHAareHAyou?"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	s = "あいうえおあいあいうえおあい"
+	fmt.Printf("%#v\n", piscine.Split(s, "あい"))
+	s = "HAHAHAHAHAHA"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+}
