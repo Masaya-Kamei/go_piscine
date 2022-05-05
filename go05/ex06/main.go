bb92c974d56fe501847eb0663f575402a5b4d81f
Masaya Kamei <km9smc6@gmail.com>
2022-05-05 14:08:05 +0900

    Edit ex06 main

--- go05/ex06/main.go
+++ go05/ex06/main.go
@@ -9,12 +9,12 @@ func main() {
 	s := "HelloHAhowHAareHAyou?"
 	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
 
-	s = "HAHelloHAhowHAareHAyou?HA"
-	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
-	s = "HelloHAHAHAHAHAhowHAareHAyou?"
-	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
-	s = "あいうえおあいあいうえおあい"
-	fmt.Printf("%#v\n", piscine.Split(s, "あい"))
-	s = "HAHAHAHAHAHA"
-	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	// s = "HAHelloHAhowHAareHAyou?HA"
+	// fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	// s = "HelloHAHAHAHAHAhowHAareHAyou?"
+	// fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	// s = "あいうえおあいあいうえおあい"
+	// fmt.Printf("%#v\n", piscine.Split(s, "あい"))
+	// s = "HAHAHAHAHAHA"
+	// fmt.Printf("%#v\n", piscine.Split(s, "HA"))
 }
