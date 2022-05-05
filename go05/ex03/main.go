ab22d83c28f603cf50a1a4de60bf28a8d793349b
Masaya Kamei <km9smc6@gmail.com>
2022-05-05 14:04:22 +0900

    Edit ex03 main

--- go05/ex03/main.go
+++ go05/ex03/main.go
@@ -10,6 +10,7 @@ func main() {
 
 	// fmt.Printf("%#v\n", piscine.SplitWhiteSpaces("  Hello how are you?  "))
 	// fmt.Printf("%#v\n", piscine.SplitWhiteSpaces("Hello  how  \nare \tyou?"))
+	// fmt.Printf("%#v\n", piscine.SplitWhiteSpaces(" あい  うえ "))
 	// fmt.Printf("%#v\n", piscine.SplitWhiteSpaces("    \t\n"))
 	// fmt.Printf("%#v\n", piscine.SplitWhiteSpaces(""))
 }
