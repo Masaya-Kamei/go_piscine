5a6291e2a6c8733a6511c0569fb29e41b7334159
Masaya Kamei <km9smc6@gmail.com>
2022-05-08 17:46:21 +0900

    Add ex05

---
+++ go08/ex05/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "ft"
+
+type Door struct {
+	state int
+}
+
+const (
+	OPEN int = iota
+	CLOSE
+)
+
+func PrintStr(s string) {
+	for _, r := range s {
+		ft.PrintRune(r)
+	}
+	ft.PrintRune('\n')
+}
+
+func OpenDoor(ptrDoor *Door) {
+	PrintStr("Door Opening...")
+	ptrDoor.state = OPEN
+}
+
+func CloseDoor(ptrDoor *Door) {
+	PrintStr("Door Closing...")
+	ptrDoor.state = CLOSE
+}
+
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("is the Door opened ?")
+	return ptrDoor.state == OPEN
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	return ptrDoor.state == CLOSE
+}
+
+func main() {
+	door := &Door{}
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+	if door.state == OPEN {
+		CloseDoor(door)
+	}
+}
