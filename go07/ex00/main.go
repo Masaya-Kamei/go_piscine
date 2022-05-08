a36c55d9d10a70d5b76af51c8f48bedee3b14408
Masaya Kamei <km9smc6@gmail.com>
2022-05-08 11:33:27 +0900

    Add ex00

---
+++ go07/ex00/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"ft"
+	"piscine"
+)
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6}
+	piscine.ForEach(piscine.PrintNbr, a)
+	ft.PrintRune('\n')
+
+	ft.PrintRune('\n')
+	a = []int{0, -9223372036854775808, 9223372036854775807}
+	piscine.ForEach(piscine.PrintNbr, a)
+	ft.PrintRune('\n')
+	piscine.ForEach(piscine.PrintNbr, nil)
+	ft.PrintRune('\n')
+}
