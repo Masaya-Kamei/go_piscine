91042ec4d689c41bd220eb7873666003b0b6b3e7
Masaya Kamei <km9smc6@gmail.com>
2022-05-08 10:39:44 +0900

    Fix ex06

--- go05/ex03/main.go
+++ go05/ex03/main.go
@@ -10,14 +10,15 @@ func testSplitSpace(s string) {
 	result := piscine.SplitWhiteSpaces(s)
 	real := regexp.MustCompile("[ \t\n]").Split(s, -1)
 	fmt.Printf("%#v\n", result)
+	if len(result) != len(real) {
+		fmt.Printf("%#v	<- KO\n", real)
+		return
+	}
 	for i := range result {
 		if result[i] != real[i] {
 			fmt.Printf("%#v	<- KO\n", real)
 			return
 		}
-	}
-	if len(result) != len(real) {
-		fmt.Printf("%#v	<- KO\n", real)
 	}
 }
 
--- go05/ex06/main.go
+++ go05/ex06/main.go
@@ -3,18 +3,37 @@ package main
 import (
 	"fmt"
 	"piscine"
+	"strings"
 )
+
+func testSplit(s, sep string) {
+	result := piscine.Split(s, sep)
+	real := strings.Split(s, sep)
+	fmt.Printf("%#v\n", result)
+	if len(result) != len(real) {
+		fmt.Printf("%#v	<- KO\n", real)
+		return
+	}
+	for i := range result {
+		if result[i] != real[i] {
+			fmt.Printf("%#v	<- KO\n", real)
+			return
+		}
+	}
+}
 
 func main() {
 	s := "HelloHAhowHAareHAyou?"
 	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
 
-	// s = "HAHelloHAhowHAareHAyou?HA"
-	// fmt.Printf("%#v\n", piscine.Split(s, "HA"))
-	// s = "HelloHAHAHAHAHAhowHAareHAyou?"
-	// fmt.Printf("%#v\n", piscine.Split(s, "HA"))
-	// s = "あいうえおあいあいうえおあい"
-	// fmt.Printf("%#v\n", piscine.Split(s, "あい"))
-	// s = "HAHAHAHAHAHA"
-	// fmt.Printf("%#v\n", piscine.Split(s, "HA"))
+	fmt.Println()
+	testSplit("HelloHAhowHAareHAyou?", "HA")
+	testSplit("HAHelloHAhowHAareHAyou?HA", "HA")
+	testSplit("HelloHAHAHAHAHAhowHAareHAyou?", "HA")
+	testSplit("あいうえおあいあいうえおあい", "あい")
+	testSplit("HAHAHAHAHAHA", "HA")
+	testSplit("", "HA")
+	testSplit("あいうえお", "")
+	testSplit("HA", "")
+	testSplit("", "")
 }
