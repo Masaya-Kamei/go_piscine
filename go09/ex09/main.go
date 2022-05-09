041bd8dfc5626687289d6857e0c33ac5b48e5ad2
Masaya Kamei <km9smc6@gmail.com>
2022-05-09 14:31:42 +0900

    Edit ex09 main

--- go09/ex09/main.go
+++ go09/ex09/main.go
@@ -22,15 +22,22 @@ func main() {
 	fmt.Println(found, *found)
 	found = piscine.ListFind(link, "hello5", piscine.CompStr)
 	fmt.Println(found)
-	found = piscine.ListFind(link, []byte("hello2"), piscine.CompStr)
+
+	piscine.ListPushBack(link, 1)
+	found = piscine.ListFind(link, 1, piscine.CompStr)
+	fmt.Println(found, *found)
+	found = piscine.ListFind(link, float64(1), piscine.CompStr)
 	fmt.Println(found)
 
 	piscine.ListPushBack(link, nil)
-	found = piscine.ListFind(link, interface{}(nil), piscine.CompStr)
-	fmt.Println(found, *found)
 	found = piscine.ListFind(link, nil, piscine.CompStr)
 	fmt.Println(found, *found)
+	var a *int = nil
+	found = piscine.ListFind(link, a, piscine.CompStr)
+	fmt.Println(found)
 
 	found = piscine.ListFind(nil, interface{}("hello2"), piscine.CompStr)
+	fmt.Println(found)
 	found = piscine.ListFind(link, interface{}("hello2"), nil)
+	fmt.Println(found)
 }
