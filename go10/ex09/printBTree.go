dfe49edcf317d80517e6f2461d1d00e9eb2cfa6f
mkamei <mkamei@student.42tokyo.jp>
2022-05-12 12:20:21 +0900

    Edit printBTree 2

--- go10/ex09/printBTree.go
+++ go10/ex09/printBTree.go
@@ -2,37 +2,32 @@ package main
 
 import (
 	"fmt"
-	"ft"
 	"piscine"
 )
 
 type TreeNode = piscine.TreeNode
 
-const maxDepth = 4
-const maxLength int = 2 * 2 * 2
-
-func storeToTable(tn *TreeNode, table [][maxLength]*TreeNode, depth, i int) {
-	if tn == nil || depth == maxDepth {
-		return
-	}
-	table[depth][i] = tn
-	storeToTable(tn.Left, table, depth+1, i*2)
-	storeToTable(tn.Right, table, depth+1, i*2+1)
-}
-
-func gD(n *TreeNode) string {
-	if n == nil {
-		return " "
-	}
-	return n.Data
-}
-
-func printTreeNode(n *TreeNode) int {
-	if n == nil {
-		return 0
+func printTreeNode(n *TreeNode) {
+	gD := func(n *TreeNode) string {
+		if n == nil {
+			return " "
+		}
+		return n.Data
 	}
 	fmt.Printf("(%v->)%v->[%v, %v] ", gD(n.Parent), n.Data, gD(n.Left), gD(n.Right))
-	return 1
+}
+
+func printTreeNodeLevel(root *TreeNode, depth int) bool {
+	if root == nil {
+		return false
+	}
+	if depth == 0 {
+		printTreeNode(root)
+		return true
+	}
+	ok1 := printTreeNodeLevel(root.Left, depth-1)
+	ok2 := printTreeNodeLevel(root.Right, depth-1)
+	return ok1 || ok2
 }
 
 func printBTree(root *TreeNode) {
@@ -40,16 +35,11 @@ func printBTree(root *TreeNode) {
 		fmt.Println("nil")
 		return
 	}
-	table := [maxDepth][maxLength]*TreeNode{}
-	storeToTable(root, table[:], 0, 0)
-	for i := 0; i < maxDepth; i++ {
-		existFlag := 0
-		for j := 0; j < maxLength; j++ {
-			existFlag |= printTreeNode(table[i][j])
+	ok := true
+	for depth := 0; ok; depth++ {
+		if depth != 0 {
+			fmt.Println()
 		}
-		if existFlag == 0 {
-			break
-		}
-		ft.PrintRune('\n')
+		ok = printTreeNodeLevel(root, depth)
 	}
 }
