4982d69c110eecc4b4bfd723e23bff34e28154f0
mkamei <mkamei@student.42tokyo.jp>
2022-05-14 11:17:32 +0900

    Edit ex00

--- go08/ex00/vendor/piscine/rot14.go
+++ go08/ex00/vendor/piscine/rot14.go
@@ -1,14 +1,12 @@
 package piscine
 
 func Rot14(s string) string {
-	alphabetLarge := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	alphabetSmall := "abcdefghijklmnopqrstuvwxyz"
 	rs := []rune(s)
 	for i, r := range rs {
 		if r >= 'A' && r <= 'Z' {
-			rs[i] = rune(alphabetLarge[(r-'A'+14)%26])
+			rs[i] = (r-'A'+14)%26 + 'A'
 		} else if r >= 'a' && r <= 'z' {
-			rs[i] = rune(alphabetSmall[(r-'a'+14)%26])
+			rs[i] = (r-'a'+14)%26 + 'a'
 		}
 	}
 	return string(rs)
