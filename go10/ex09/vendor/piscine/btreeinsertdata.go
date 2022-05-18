46719a61b86f10b16d6b735f2f4d5b976e7dc0f2
mkamei <mkamei@student.42tokyo.jp>
2022-05-18 13:56:02 +0900

    Edit ex00

--- go10/ex09/vendor/piscine/btreeinsertdata.go
+++ go10/ex09/vendor/piscine/btreeinsertdata.go
@@ -1,10 +1,15 @@
 package piscine
 
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		return &TreeNode{Data: data}
 	}
-	if data < root.Data {
+	if data <= root.Data {
 		if root.Left == nil {
 			root.Left = &TreeNode{Data: data, Parent: root}
 		} else {
